285b5f071c54466649589bb5ff249b508da685f9
José Carlos Chávez <jcchavezs@gmail.com>
2021-07-07 12:47:50 +0200

    fix: fixes grpc config.

--- config/configgrpc/configgrpc.go
+++ config/configgrpc/configgrpc.go
@@ -51,6 +51,10 @@ var (
 	}
 )
 
+var (
+	clientOptionHandlerList = make([]ClientDialOptionHandler, 0)
+)
+
 // Allowed balancer names to be set in grpclb_policy to discover the servers.
 var allowedBalancerNames = []string{roundrobin.Name, grpc.PickFirstBalancerName}
 
